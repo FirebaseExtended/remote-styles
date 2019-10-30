e78a80d43811c558987a23fac79056013e08bba6
David East <deast@google.com>
2019-10-30 16:31:31 -0600

    bring back loader tests

---
+++ cypress/integration/loader.styles.js
@@ -0,0 +1,52 @@
+/**
+ * @license
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/// <reference types="Cypress" />
+
+context('Remote Styles - Loader', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:5033/loader.html');
+  });
+
+  it('cy.title() - get the title', () => {
+    cy.title().should('include', 'Integration Tests');
+  });
+
+  describe('Window tests', () => {
+    it('window.remoteStyles - should have the remote styles window object', () => {
+      cy.wait(10000);
+      cy.window().should('have.property', 'remoteStyles');
+      cy.window().should('have.property', 'firebase');
+    });
+  });
+
+  describe('Added styles', () => {
+    it('should make the hello div red (rgb(255, 0, 0))', () => {
+      cy.get('#hello').should('have.css', 'color', 'rgb(255, 0, 0)');
+    });
+
+    it('should make the div 2em/32px in font-size', () => {
+      cy.get('#hello').should('have.css', 'font-size', '32px');
+    });
+
+    it('should make the div bold', () => {
+      cy.get('#hello').should('have.css', 'font-weight', 'bold');
+    });
+  })
+
+});
